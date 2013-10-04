8fdb82ad2f8868a272eb88e3614f08908b9da652
Will Spearing <nwspearing@gmail.com>
2013-10-03 17:16:00 -0700

    MainCtrl works. Win conditions functioning.  Start-again after win/tie not working yet.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,77 +1,61 @@
-'use strict';
+// 'use strict';
 
-angular.module('newTicApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = "Whatever";
-      
-  });
+angular.module('newTicApp').controller('MainCtrl', function($scope) {
 
+   var player_turn = 1;
 
-// angular.module('newTicApp')
-//   .controller('MainCtrl', function ($scope) {
-//     $scope.awesomeThings = [
-//       'ice-cream - HTML5 Boilerplate',
-//       'beer - AngularJS',
-//       'chocolate - Karma'
-//     ];
-//   });
+$scope.ticTacToe= [['','',''],['','',''],['','','']];
+
+$scope.clickSquare = function(row, column) { 
+   
 
 
+    // console.log(row);
+    // console.log(column);
 
-
-// in html
-// ng-app
-// ng-controller
-
-// and referenced here
-
-// <script>
-
-
-    var player_turn = 1;
-
-
-var cellArray = [0,1,2,3,4,5,6,7,8];
-
-
-
-
-
-    function playBox() {
-
-
-    	// WHAT HAPPENS IF YOU WIN / TIE
-
-         
-
-      function win()
-       {
-
-        document.getElementById("message_overlay").style.zIndex = "2";
-        document.getElementById("message_overlay").innerHTML = "Player "+ ((player_turn % 2)+1) + " wins! " +"<br/>"+ "<input type='button' id='btn2' value='Start Again' onclick='startAgain()'/>";
-        document.getElementById("notification").innerHTML = "";
      
-        }
 
-        function tie()
-        {
-           document.getElementById("message_overlay").style.zIndex = "2";
-           document.getElementById("message_overlay").innerHTML = "You both tied! " +"<br/>"+ "<input type='button' id='btn2' value='Start Again' onclick='startAgain()'/>";
-           document.getElementById("notification").innerHTML = "";
+     // $scope.cell.innerHTML = "X";
+     //      this.turnNum++;
+     // if(this.turnNum % 2 = 1)
 
-        }
+     // this.ticTacToe[row][column]= "X";
+     // else
+     //  this.ticTacToe[row][column]="O";
+      // event.target.innerHTML = "<img src='batman.jpg'>"
+
+
+
+
+
+ 
+
+
+// var cellArray = [0,1,2,3,4,5,6,7,8];
+
+
+
+
+
+    // function playBox() {
 
 
       if (event.target.innerHTML != "X" && event.target.innerHTML != "O") {
 
       if (player_turn % 2) {
 
-      event.target.innerHTML="X";
+      // event.target.innerHTML="X";
+      this.ticTacToe[row][column]= "X";
+      
       }
 
 
     else {
-    event.target.innerHTML = "O";
+  
+    // event.target.innerHTML = "O";
+
+    this.ticTacToe[row][column]="O";
+     
     }
     
     document.getElementById("notification").innerHTML = "Player "+((player_turn % 2)+1)+" - your turn!";
@@ -80,19 +64,38 @@ var cellArray = [0,1,2,3,4,5,6,7,8];
 
 
 
- 	var i;
-      for (i=0; i<9; ++i)
-      {
+
+    //   for (i=0; i<9; ++i)
+    //   {
    
-      cellArray[i] =  document.getElementById("cell1_"+(i+1)).innerHTML;
+    //   cellArray[i] =  document.getElementById("cell1_"+(i+1)).innerHTML;
 
     
-    }   
+    // }   
 
   
 
    
-    
+    // WHAT HAPPENS IF YOU WIN / TIE
+
+         
+
+      function win()
+       {
+
+        document.getElementById("message_overlay").style.zIndex = "2";
+        document.getElementById("message_overlay").innerHTML = "Player "+ ((player_turn % 2)+1) + " wins! " +"<br/>"+ "<input type='button' id='btn2' value='Start Again' ng-click='startAgain()'/>";
+        document.getElementById("notification").innerHTML = "";
+     
+        }
+
+        function tie()
+        {
+           document.getElementById("message_overlay").style.zIndex = "2";
+           document.getElementById("message_overlay").innerHTML = "You both tied! " +"<br/>"+ "<input type='button' id='btn2' value='Start Again' ng-click='startAgain()'/>";
+           document.getElementById("notification").innerHTML = "";
+
+        }
 
       // end test to see if they clicked an already filled block
      } 
@@ -104,66 +107,54 @@ var cellArray = [0,1,2,3,4,5,6,7,8];
         {
 
 
-        
-         
-          if (cellArray[0] == cellArray[3] && cellArray[3] != "" && cellArray[3] == cellArray[6])
-              win();
+          
             
-          else if (cellArray[1] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[7])
+
+          
+          if (this.ticTacToe[0][0] == this.ticTacToe[0][1] && this.ticTacToe[0][1]  != "" && this.ticTacToe[0][1]  == this.ticTacToe[0][2] )
               win();
 
-          else if (cellArray[2] == cellArray[5] && cellArray[5] != "" && cellArray[5] == cellArray[8])
-            { 
-             
+           if (this.ticTacToe[1][0] == this.ticTacToe[1][1] && this.ticTacToe[1][1]  != "" && this.ticTacToe[1][1]  == this.ticTacToe[1][2] )
               win();
-            }
-          else if (cellArray[0] == cellArray[1] && cellArray[1] != "" && cellArray[1] == cellArray[2])
-            { 
-            
+          if (this.ticTacToe[2][0] == this.ticTacToe[2][1] && this.ticTacToe[2][1]  != "" && this.ticTacToe[2][1]  == this.ticTacToe[2][2] )
               win();
-            }
-          else if (cellArray[3] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[5])
-            { 
-            
+          if (this.ticTacToe[0][0] == this.ticTacToe[1][0] && this.ticTacToe[1][0]  != "" && this.ticTacToe[1][0]  == this.ticTacToe[2][0] )
               win();
-            }
-          else if (cellArray[6] == cellArray[7] && cellArray[7] != "" && cellArray[7] == cellArray[8])
-            { 
-             
+          if (this.ticTacToe[0][1] == this.ticTacToe[1][1] && this.ticTacToe[1][1]  != "" && this.ticTacToe[1][1]  == this.ticTacToe[2][1] )
+              win();  
+          if (this.ticTacToe[0][2] == this.ticTacToe[1][2] && this.ticTacToe[1][2]  != "" && this.ticTacToe[1][2]  == this.ticTacToe[2][2] )
+              win(); 
+            if (this.ticTacToe[0][0] == this.ticTacToe[1][1] && this.ticTacToe[1][1]  != "" && this.ticTacToe[1][1]  == this.ticTacToe[2][2] )
               win();
-            }
-          else if (cellArray[0] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[8])
-            { 
-            
+             if (this.ticTacToe[0][2] == this.ticTacToe[1][1] && this.ticTacToe[1][1]  != "" && this.ticTacToe[1][1]  == this.ticTacToe[2][0] )
               win();
-            }
-          else if (cellArray[2] == cellArray[4] && cellArray[4] != "" && cellArray[4] == cellArray[6])
-            { 
-           
-              win();
-            } 
+
+       
           else if (player_turn > 9)
             {
              
               tie(); 
 
             }
+              
+            // end testing for win
 
           }
 
        // end playBox
-  }
+  // }
+
+  // end TicTacCtrl
+
+   }
 
 
-
-
-    function startAgain() {
+    $scope.startAgain =function() {
 
     location.href="index.html";
+    
+     };
 
-  }
 
+});
 
-  
-
-// </script>
